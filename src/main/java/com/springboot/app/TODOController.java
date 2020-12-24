3324ad99fd155e083c4ce085ea2ded289c5e5828
ayoubbellaj <73424140+ayoubbellaj@users.noreply.github.com>
2020-12-24 22:31:50 +0100

    Update TODOController.java

--- src/main/java/com/springboot/app/TODOController.java
+++ src/main/java/com/springboot/app/TODOController.java
@@ -2,6 +2,7 @@ package com.springboot.app;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,26 +14,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TODOController {
 
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/tasks")
 	public List<TODO> getAllTasks() {
 		return TODOService.getAllTasks();
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/tasks/{taskId}")
 	public TODO getTaskById(@PathVariable Long taskId) {
 		return TODOService.getTaskById(taskId);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/addtask")
 	public TODO addTask(@RequestBody TODO task) {
 		return TODOService.addTask(task);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PutMapping("/updateTask/{taskId}")
 	public TODO updateTask(@PathVariable Long taskId, @RequestBody TODO task) {
 		return TODOService.updateTask(taskId, task);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@DeleteMapping("/deleteTask/{taskId}")
 	public TODO deleteTask(@PathVariable Long taskId) {
 		return TODOService.deleteTask(taskId);
